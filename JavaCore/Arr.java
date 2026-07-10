b3499d56c439a6356049d6cc3012031fd573c5b7
Junior <eecfredes@gmail.com>
2026-07-09 21:49:43 -0300

    :rocket:different types of arrays and loops in Java

---
+++ JavaCore/Arr.java
@@ -0,0 +1,15 @@
+package JavaCore;
+
+public class Arr {
+    public static void main(String a[]) {
+        int nums[] = new int[7];
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
