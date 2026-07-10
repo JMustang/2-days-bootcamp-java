e9bd84468a911a77659be47115465354f98bd28a
Junior <eecfredes@gmail.com>
2026-07-10 20:42:31 -0300

    :rocket:A little about strings

---
+++ JavaCore/Navin.java
@@ -0,0 +1,13 @@
+package JavaCore;
+
+public class Navin {
+    public static void main(String a[]) {
+        // String name = new String("navin");
+
+        String name = "navin";
+        name = name + " reddy";
+
+        System.out.println(name);
+        // System.out.println(name.concat(" reddy"));
+    }
+}
